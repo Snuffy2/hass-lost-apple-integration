02d973bddc55820e72d75efa0eabf2da2e95f2ef
Snuffy2 <Snuffy2@users.noreply.github.com>
2026-05-23 23:45:43 -0400

    Prevent integration version skew

--- custom_components/lost_apple/const.py
+++ custom_components/lost_apple/const.py
@@ -5,5 +5,6 @@ from __future__ import annotations
 from typing import Final
 
 DOMAIN: Final = "lost_apple"
+VERSION: Final = "0.1.0"
 CONF_BASE_URL: Final = "base_url"
 CONF_PAIRING_TOKEN: Final = "pairing_token"  # noqa: S105
